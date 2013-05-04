d5a56566ba50798f7ae3864ea5444f523329d84c
bn0x <0_bn0x@hushmail.com>
2013-05-04 15:03:42 -0700

    Ranked stats!

--- Elophant.py
+++ Elophant.py
@@ -5,18 +5,19 @@ Import our modules
 import urllib2
 import urllib
 import json
+import socket
 
 """
 Make sure to put in your API Key
 """
-apiKey = 'Put your API Key here'
+apiKey = 'Put in your API Key'
 
 """
 The function to get summoner stats and return them.
 'fail' is the only way to detect if an exception occured until I find another way.
 """
 
-def SummonerStats(region, requestData):
+def summonerStats(region, requestData):
 	try:
 		if ' ' in requestData:
 			requestData = requestData.replace(' ', '%20')
@@ -26,3 +27,32 @@ def SummonerStats(region, requestData):
 		return { 'summonerLevel': summonerJSON['data']['summonerLevel'], 'name': summonerJSON['data']['name'], 'internalName': summonerJSON['data']['internalName'], 'revisionDate': summonerJSON['data']['revisionDate'], 'profileIconId': summonerJSON['data']['profileIconId'], 'acctId': summonerJSON['data']['acctId'], 'summonerId': summonerJSON['data']['summonerId'] }
 	except:
 		return { 'fail': 'Failed to get summoner stats.' }
+
+def summonerLeagues(region, summonerName):
+		try:
+			if ' ' in summonerName:
+				summonerName = summonerName.replace(' ', '%20')
+			apiURL = 'http://api.elophant.com/v2/' + region + '/' + 'summoner' + '/' + summonerName + '?key=' + apiKey
+			summonerURLOpen = urllib2.urlopen(apiURL)
+			summonerJSON = json.load(summonerURLOpen)
+			summonerID = summonerJSON['data']['summonerId']
+			leagueURL = 'http://api.elophant.com/v2/' + region + '/' + 'leagues' + '/' + str(summonerID) + '?key=' + apiKey
+			leagueURLOpen = urllib2.urlopen(leagueURL)
+			leagueJSON = json.load(leagueURLOpen)
+			tier = leagueJSON['data']['summonerLeagues'][0]['tier']
+			league = leagueJSON['data']['summonerLeagues'][0]['name']
+			rank = leagueJSON['data']['summonerLeagues'][0]['entries'][0]['rank']
+			previousDayLeaguePosition = leagueJSON['data']['summonerLeagues'][0]['entries'][0]['previousDayLeaguePosition']
+			hotStreak = leagueJSON['data']['summonerLeagues'][0]['entries'][0]['hotStreak']
+			freshBlood = leagueJSON['data']['summonerLeagues'][0]['entries'][0]['freshBlood']
+			lastPlayed = leagueJSON['data']['summonerLeagues'][0]['entries'][0]['lastPlayed']
+			playerOrTeamId = leagueJSON['data']['summonerLeagues'][0]['entries'][0]['playerOrTeamId']
+			inactive = leagueJSON['data']['summonerLeagues'][0]['entries'][0]['inactive']
+			veteran = leagueJSON['data']['summonerLeagues'][0]['entries'][0]['veteran']
+			queueType = leagueJSON['data']['summonerLeagues'][0]['entries'][0]['queueType']
+			losses = leagueJSON['data']['summonerLeagues'][0]['entries'][0]['losses']
+			playerOrTeamName = leagueJSON['data']['summonerLeagues'][0]['entries'][0]['playerOrTeamName']
+			wins = leagueJSON['data']['summonerLeagues'][0]['entries'][0]['wins']
+			return { 'summonerName': summonerName, 'tier': tier, 'league': league, 'rank': rank, 'previousDayLeaguePosition': previousDayLeaguePosition, 'hotStreak': hotStreak, 'freshBlood': freshBlood, 'lastPlayed': lastPlayed, 'playerOrTeamId': playerOrTeamId, 'inactive': inactive, 'veteran': veteran, 'queueType': queueType, 'losses': losses, 'playerOrTeamName': playerOrTeamName, 'wins': wins }
+		except socket.error:
+			return { 'fail': 'Failed to get ranked stats.' }
--- Module Testing.py
+++ Module Testing.py
@@ -14,8 +14,27 @@ region = raw_input("Region: ")
 """
 Call the function to get data.
 """
+try:
+	if stats['fail']:
+		print stats['fail']
+except:
+	pass
 
-stats = Elophant.SummonerStats(region, summonerName)
+tier = Elophant.summonerLeagues(region, summonerName)
+print(' ')
+print summonerName, 'ranked stats: '
+print tier['tier'], tier['league'], tier['rank']
+print tier['previousDayLeaguePosition']
+print tier['hotStreak']
+print tier['freshBlood']
+print tier['lastPlayed']
+print tier['playerOrTeamId']
+print tier['inactive']
+print tier['veteran']
+print tier['queueType']
+print tier['losses']
+print tier['playerOrTeamName']
+print tier['wins']
 
 """
 Check if it failed. (Python exception)
@@ -28,6 +47,9 @@ except:
 """
 Print the output from the API
 """
+stats = Elophant.summonerStats(region, summonerName)
+print '\n'
+print 'Summoner stats of:', summonerName
 print stats['summonerLevel']
 print stats['name']
 print stats['internalName']
