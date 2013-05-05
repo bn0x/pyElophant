20680b07137cebf11f6c7c4fe31667b5ba53e157
bn0x <0_bn0x@hushmail.com>
2013-05-05 00:36:28 -0700

    Some better looking code

--- Elophant.py
+++ Elophant.py
@@ -5,12 +5,11 @@ Import our modules
 import urllib2
 import urllib
 import json
-import socket
 
 """
 Make sure to put in your API Key
 """
-apiKey = ''
+apiKey = 'Your api key'
 
 """
 The function to get summoner stats and return them.
@@ -21,10 +20,13 @@ def summonerStats(region, requestData):
 	try:
 		if ' ' in requestData:
 			requestData = requestData.replace(' ', '%20')
+		
 		apiURL = 'http://api.elophant.com/v2/' + region + '/' + 'summoner' + '/' + requestData + '?key=' + apiKey
 		summonerURLOpen = urllib2.urlopen(apiURL)
 		summonerJSON = json.load(summonerURLOpen)
+		
 		return { 'summonerLevel': summonerJSON['data']['summonerLevel'], 'name': summonerJSON['data']['name'], 'internalName': summonerJSON['data']['internalName'], 'revisionDate': summonerJSON['data']['revisionDate'], 'profileIconId': summonerJSON['data']['profileIconId'], 'acctId': summonerJSON['data']['acctId'], 'summonerId': summonerJSON['data']['summonerId'] }
+	
 	except:
 		return { 'fail': 'Failed to get summoner stats.' }
 
@@ -39,18 +41,24 @@ def summonerLeagues(region, summonerName):
 	try:
 		if ' ' in summonerName:
 			summonerName = summonerName.replace(' ', '%20')
+		
 		apiURL = 'http://api.elophant.com/v2/' + region + '/' + 'summoner' + '/' + summonerName + '?key=' + apiKey
 		summonerURLOpen = urllib2.urlopen(apiURL)
 		summonerJSON = json.load(summonerURLOpen)
+		
 		summonerID = summonerJSON['data']['summonerId']
+		
 		leagueURL = 'http://api.elophant.com/v2/' + region + '/' + 'leagues' + '/' + str(summonerID) + '?key=' + apiKey
 		leagueURLOpen = urllib2.urlopen(leagueURL)
 		leagueJSON = json.load(leagueURLOpen)
+		
 		tier = leagueJSON['data']['summonerLeagues'][0]['tier']
 		league = leagueJSON['data']['summonerLeagues'][0]['name']
 		playerOrTeamName = leagueJSON['data']['summonerLeagues'][0]['entries'][0]['playerOrTeamName']
 		requestorsRank = leagueJSON['data']['summonerLeagues'][0]['requestorsRank']
+		
 		return { 'requestorsRank': requestorsRank, 'summonerName': summonerName, 'tier': tier, 'league': league, 'rank': rank }
+	
 	except:
 		return { 'fail': 'User is NOT ranked or an error has occured.' }
 
@@ -86,5 +94,6 @@ def teamLookup(region, teamName):
 			return { 'fail': 'Failed to find team\'s 5v5 ranked record.' }
 
 		return { 'losses': losses, 'wins': wins}
+	
 	except:
 		return { 'fail': 'Failed to find team\'s 5v5 ranked record.' }
